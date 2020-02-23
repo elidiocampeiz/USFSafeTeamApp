464db112d354129985a9a05688d37be128930ef7
jackyang <jackyang@mail.usf.edu>
2020-02-23 02:37:02 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/usfsafeteamapp/MainActivity.java

--- app/src/main/java/com/example/usfsafeteamapp/MainActivity.java
+++ app/src/main/java/com/example/usfsafeteamapp/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.usfsafeteamapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+        Button B;
+    Button B1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        B = findViewById(R.id.buttonClient);
+
+        B.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ClientHome.class);
+                startActivity(i);
+            }
+        });
+
+        B1 = findViewById(R.id.buttonDriver);
+
+        B1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, DriverHome.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
