94c7a656a1b4744110e5516572c4f9997bf0be05
jackyang <jackyang@mail.usf.edu>
2020-02-23 17:24:14 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/usfsafeteamapp/MainActivity.java

--- app/src/main/java/com/example/usfsafeteamapp/ClientHome.java
+++ app/src/main/java/com/example/usfsafeteamapp/ClientHome.java
@@ -1,14 +1,29 @@
 package com.example.usfsafeteamapp;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * This shows how to create a simple activity with a raw MapView and add a marker to it. This
@@ -16,81 +31,129 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class ClientHome extends AppCompatActivity implements OnMapReadyCallback {
 
-    private MapView mMapView;
+    private GoogleMap mMap;
+    LocationManager locm;
 
-    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
-
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_home);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_container);
+        mapFragment.getMapAsync(this);
 
-        // *** IMPORTANT ***
-        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
-        // objects or sub-Bundles.
-        Bundle mapViewBundle = null;
-        if (savedInstanceState != null) {
-            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        locm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+            return;
         }
-        mMapView = (MapView) findViewById(R.id.user_list_map);
-        mMapView.onCreate(mapViewBundle);
+        if(locm.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+        {
+            locm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener()
+            {
 
-        mMapView.getMapAsync(this);
+                @Override
+                public void onLocationChanged(Location location) {
+                    double lat = location.getLatitude();
+                    double lon = location.getLongitude();
+                    LatLng coords = new LatLng(lat,lon);
 
-    }
+                    Geocoder geo = new Geocoder(getApplicationContext());
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+                    try{
+                        List<Address> list = geo.getFromLocation(lat,lon,1);
 
-        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
-        if (mapViewBundle == null) {
-            mapViewBundle = new Bundle();
-            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+                        String str = list.get(0).getLocality() + ", ";
+                        str += list.get(0).getCountryName();
+
+                        mMap.addMarker(new MarkerOptions().position(coords).title("This is my position"));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 8.2f));
+                    }
+                    catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
+
+        }
+        else if(locm.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            locm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double lat = location.getLatitude();
+                    double lon = location.getLongitude();
+
+                    LatLng coords = new LatLng(lat,lon);
+
+                    Geocoder geo = new Geocoder(getApplicationContext());
+
+                    try{
+                        List<Address> list = geo.getFromLocation(lat,lon,1);
+                        String str = list.get(0).getLocality() + ", ";
+                        str += list.get(0).getCountryName();
+
+                        mMap.addMarker(new MarkerOptions().position(coords).title("This is my position"));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 8.2f));
+                    }
+                    catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
         }
 
-        mMapView.onSaveInstanceState(mapViewBundle);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mMapView.onResume();
-    }
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
-    protected void onStart() {
-        super.onStart();
-        mMapView.onStart();
-    }
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mMapView.onStop();
-    }
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-    }
-
-    @Override
-    protected void onPause() {
-        mMapView.onPause();
-        super.onPause();
-    }
-
-    @Override
-    protected void onDestroy() {
-        mMapView.onDestroy();
-        super.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mMapView.onLowMemory();
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
 }
