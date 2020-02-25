3a000fd60d2470acf4416d7f604fcf7b424cbb3f
Jose Elidio Campeiz Neto <jecampeiz@gmail.com>
2020-02-25 02:50:30 -0500

    commit frontend6

--- app/src/main/java/com/example/usfsafeteamapp/Client/ClientHome.java
+++ app/src/main/java/com/example/usfsafeteamapp/Client/ClientHome.java
@@ -1,40 +1,64 @@
 package com.example.usfsafeteamapp.Client;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Icon;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.usfsafeteamapp.R;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.AutocompleteFragment;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class ClientHome extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    SupportMapFragment mapFragment;
     LocationManager locm;
-    EditText ET;
-    MarkerOptions mkr;
+    LatLng curr_coords, dest_coords;
+    //28.063959, -82.413417
+    final LatLng msc_LatLng = new LatLng(28.0639,-82.4134);
+    MarkerOptions curr_mkr, dest_mkr;
 
-
+    int AUTOCOMPLETE_REQUEST_CODE = 1;
+    String TAG;
+    Place destination;
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,21 +66,29 @@ public class ClientHome extends AppCompatActivity implements OnMapReadyCallback 
         setContentView(R.layout.activity_client_home);
 
         Button B = findViewById(R.id.buttonConfirm);
-        ET = findViewById(R.id.textView2);
+
 
         B.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(ClientHome.this, ClientWait.class);
-                String str = ET.getText().toString();
-                i.putExtra("value", str);
+//                String str = ET.getText().toString();
+//                i.putExtra("value", str);
                 startActivity(i);
             }
         });
+        //Initialize Places API
+        setUpPlacesAPI();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment2);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment2);
         mapFragment.getMapAsync(this);
+
+
+
+        // Initialize the AutocompleteSupportFragment.
+        setUpAutocompleteSupportFragment();
+
 
         locm = (LocationManager) getSystemService(LOCATION_SERVICE);
         if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED) {
@@ -73,10 +105,12 @@ public class ClientHome extends AppCompatActivity implements OnMapReadyCallback 
                 public void onLocationChanged(Location location) {
                     double lat = location.getLatitude();
                     double lon = location.getLongitude();
-                    LatLng coords = new LatLng(lat,lon);
-                    mMap.addMarker(new MarkerOptions().position(coords).title("This is my position"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 12.2f));
 
+//                    mapFragment.getMapAsync(ClientHome.this);
+                    curr_coords = new LatLng(lat,lon );
+                    curr_mkr = new MarkerOptions().position(curr_coords).title("This is my position");
+                    mMap.addMarker(curr_mkr);
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curr_coords, 14.2f));
 //                    Geocoder geo = new Geocoder(getApplicationContext());
 //
 //                    try{
@@ -84,7 +118,7 @@ public class ClientHome extends AppCompatActivity implements OnMapReadyCallback 
 //
 //                        String str = list.get(0).getLocality() + ", ";
 //                        str += list.get(0).getCountryName();
-//                        mkr = new MarkerOptions().position(coords).title("This is my position");
+//                        mkr = new MarkerOptions().position(curr_coords).title("This is my position");
 //
 //                    }
 //                    catch(IOException e){
@@ -116,9 +150,12 @@ public class ClientHome extends AppCompatActivity implements OnMapReadyCallback 
                     double lat = location.getLatitude();
                     double lon = location.getLongitude();
 
-                    LatLng coords = new LatLng(lat,lon);
-                    mMap.addMarker(new MarkerOptions().position(coords).title("This is my position"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 12.2f));
+
+//                    mapFragment.getMapAsync(ClientHome.this);
+                    curr_coords = new LatLng(lat,lon );
+                    curr_mkr = new MarkerOptions().position(curr_coords).title("This is my position");
+                    mMap.addMarker(curr_mkr);
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curr_coords, 14.2f));
 //
 //                    Geocoder geo = new Geocoder(getApplicationContext());
 //
@@ -127,8 +164,8 @@ public class ClientHome extends AppCompatActivity implements OnMapReadyCallback 
 //                        String str = list.get(0).getLocality() + ", ";
 //                        str += list.get(0).getCountryName();
 //
-//                        mMap.addMarker(new MarkerOptions().position(coords).title("This is my position"));
-//                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 12.2f));
+//                        mMap.addMarker(new MarkerOptions().position(curr_coords).title("This is my position"));
+//                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curr_coords, 12.2f));
 //                    }
 //                    catch(IOException e){
 //                        e.printStackTrace();
@@ -150,24 +187,28 @@ public class ClientHome extends AppCompatActivity implements OnMapReadyCallback 
 
                 }
             });
+
         }
 
+//        final Context cntx = this;
+//        TextView txt2 = findViewById(R.id.textView2);
+//        txt2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Set the fields to specify which types of place data to
+//                // return after the user has made a selection.
+//                List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
+//
+//                // Start the autocomplete intent.
+//                Intent Placeintent = new Autocomplete.IntentBuilder(
+//                        AutocompleteActivityMode.OVERLAY, fields)
+//                        .build(cntx);
+//                startActivityForResult(Placeintent, AUTOCOMPLETE_REQUEST_CODE);
+//            }
+//        });
 
     }
-//    void googlePleaceSearchIntent(){
-//        int AUTOCOMPLETE_REQUEST_CODE = 1;
-//
-//    // Set the fields to specify which types of place data to
-//    // return after the user has made a selection.
-//            List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
-//
-//    // Start the autocomplete intent.
-//            Intent intent = new Autocomplete.IntentBuilder(
-//                    AutocompleteActivityMode.FULLSCREEN, fields)
-//                    .build(this);
-//        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
-//
-//    }
+
 
 
     /**
@@ -183,11 +224,92 @@ public class ClientHome extends AppCompatActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 //        mMap.addMarker(mkr);
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 12.2f));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curr_coords, 12.2f));
         // Add a marker in Sydney and move the camera
         //LatLng sydney = new LatLng(-34, 151);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        mMap.addMarker(new MarkerOptions().position(dest_coords).title("Destination!").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+
+
+
+
+
+
     }
 
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        if (mMap != null){
+//            mMap.clear();
+//        }
+//    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+//            if (resultCode == RESULT_OK) {
+//                Place place = Autocomplete.getPlaceFromIntent(data);
+//
+//                //TO DO: ADD Route
+//                //mMap.addMarker(new MarkerOptions().)
+//
+//
+//                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+//            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+//                // TODO: Handle the error.
+//                Status status = Autocomplete.getStatusFromIntent(data);
+//                Log.i(TAG, status.getStatusMessage());
+//            } else if (resultCode == RESULT_CANCELED) {
+//                // The user canceled the operation.
+//            }
+//        }
+//    }
+    public void setUpPlacesAPI(){
+        //Init Places
+        String apiKey = getString(R.string.google_maps_api_key);
+
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), apiKey);
+        }
+
+        // Create a new Places client instance.
+        PlacesClient placesClient = Places.createClient(this);
+    }
+    public void setUpAutocompleteSupportFragment(){
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected( Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+//                assert place.getLatLng()!= null;
+//                mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title("Destination: "+ place.getName()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                //dest_coords = new LatLng(place.getLatLng().latitude, place.getLatLng().longitude);
+
+
+
+//                MarkerOptions msc_mkr = new MarkerOptions().position(new LatLng(lat,lng)).title("This is my destination").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+//                mapFragment.getMapAsync(ClientHome.this);
+//                msc_LatLng
+                MarkerOptions msc_mkr = new MarkerOptions().position(msc_LatLng).title("This is my destination").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+                mMap.addMarker(msc_mkr);
+//                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(msc_LatLng, 14.2f));
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+    }
 }
